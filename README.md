1ad739228912d818aba245caee7164bc743b6029
Tazik Shahjahan <35576188+taziksh@users.noreply.github.com>
2024-03-02 10:25:51 -0500

    fix cloudfront signed url bug

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # soccer-highlights-backend
+
+## Running locally
+
+Make sure you have `key_config.py` and `soccer-highlights.pem` in the same folder.
+
+Then, run `uvicorn main:app --reload --port 8080`
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
 from typing import List
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from botocore.signers import CloudFrontSigner
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from datetime import datetime, timedelta
 from pydantic import BaseModel, Field
 import key_config as keys
 import uvicorn
 import boto3
-# running on localhost:8000
+
 app = FastAPI()
 BUCKET_NAME = 'soccer-reels-video-upload'
 s3= boto3.client('s3',
@@ -18,6 +26,34 @@ origins = [
     
 ]
 
+# AWS CloudFront settings
+cloudfront_key_pair_id = keys.CLOUDFRONT_KEY_ID
+cloudfront_distribution_domain = keys.CLOUDFRONT_DOMAIN
+cloudfront_resource_path = '/MP4/video.mp4'
+
+def rsa_signer(message):
+    with open('./cloudfront_rsa.pem', 'rb') as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+    return private_key.sign(message, padding.PKCS1v15(), hashes.SHA1())
+
+def generate_signed_url():
+    try:
+        signer = CloudFrontSigner(cloudfront_key_pair_id, rsa_signer)
+        expiration_time = datetime.now() + timedelta(hours=1)
+        # epoch_time = int(expiration_time.timestamp())
+        url = f"https://{cloudfront_distribution_domain}{cloudfront_resource_path}"
+        signed_url = signer.generate_presigned_url(
+            url,
+            date_less_than=expiration_time
+        )
+        return signed_url
+    except Exception as e:
+        print(f"ERROR: {e}")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -28,12 +64,19 @@ app.add_middleware(
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
-    if file:
+    with open(file.filename, 'rb') as file_data:
         print(file.filename)
-        s3.upload_fileobj(file.file, BUCKET_NAME, file.filename)
-        return "file uploaded"
-    else:
-        return "error in uploading."
+        try:
+            s3.upload_fileobj(file_data, BUCKET_NAME, file.filename)
+            return "file uploaded"
+        except:
+            return "error in uploading."
+    
+@app.get("/get-signed-url")
+async def get_signed_url():
+    print("LOLOLOLO")
+    signed_url = generate_signed_url()
+    return JSONResponse(content={'signedUrl': signed_url})
 
 @app.get("/")
 async def root():
